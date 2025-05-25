7b89a4fd57fd04e237b1efa17c15c3b575b0df50
ArvinJohal <arvin.u.johal@gmail.com>
2025-05-25 15:53:03 +0100

    Add files via upload

---
+++ ListMethods.py
@@ -0,0 +1,9 @@
+import random
+cars = ["BMW","Lamborghini","Bugatti","Porche","Mclaren","AstonMartin"]
+# print (cars.index("Porche"))
+# print (cars[:])
+# print (cars[:5:2])
+# print (cars[4::-1])
+# cars.sort(reverse=True)
+random.shuffle(cars)
+print(cars)
